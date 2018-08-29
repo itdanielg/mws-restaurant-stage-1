60e806df454851fafb6d14355201b30a965116ec
Daniel J George <dang81.djg@gmail.com>
2018-08-29 18:21:33 -0500

    bug: fix width issues with fixed header and absolute footer

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
-    return 'data/restaurants.json'
+    return 'data/restaurants.json'; //TODO: Change back to localhost after testing
     //return `http://localhost:${port}/data/restaurants.json`;
   }
 
